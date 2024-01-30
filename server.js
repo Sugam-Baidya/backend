c26add83338314e22aacf08877bc2a4d50670b2b
Sugam <sugambd@gmail.com>
2024-01-31 00:22:33 +0545

    final mid term

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const cors = require("cors");
 const app = express();
 const PORT = 4000;
 app.use(cors());
-// Connect to MongoDB (replace 'your_database_url' with your actual MongoDB connection string)
 mongoose.connect(
   "mongodb+srv://sugambd:sugambd@minorcluster.xlmrfo4.mongodb.net/?retryWrites=true&w=majority",
   {
@@ -23,12 +22,21 @@ const User = mongoose.model("User", {
   userType: String, // 'customer' or 'seller'
 });
 
+const Registration = mongoose.model("Registration", {
+  firstName: String,
+  lastName: String,
+  phoneNo: String,
+});
+
 app.use(bodyParser.json());
 
 // Login as Customer
 app.post("/api/login/customer", async (req, res) => {
   const { username, password } = req.body;
-
+  // Validate the request
+  if (!username || !password) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
   try {
     const customer = await User.findOne({
       username,
@@ -54,7 +62,10 @@ app.post("/api/login/customer", async (req, res) => {
 // Login as Seller
 app.post("/api/login/seller", async (req, res) => {
   const { username, password } = req.body;
-
+  // Validate the request
+  if (!username || !password) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
   try {
     const seller = await User.findOne({
       username,
@@ -82,6 +93,10 @@ app.post("/api/register/customer", async (req, res) => {
   console.log("api running");
   const { username, password } = req.body;
 
+  // Validate the request
+  if (!username || !password) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
   try {
     const existingUser = await User.findOne({ username });
 
@@ -115,6 +130,10 @@ app.post("/api/register/customer", async (req, res) => {
 app.post("/api/register/seller", async (req, res) => {
   const { username, password } = req.body;
 
+  // Validate the request
+  if (!username || !password) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
   try {
     const existingUser = await User.findOne({ username });
 
@@ -135,6 +154,42 @@ app.post("/api/register/seller", async (req, res) => {
     return res.json({
       success: true,
       message: "Seller registered successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+});
+
+// Handle registration post request
+app.post("/api/register/customer/details", async (req, res) => {
+  const { firstName, lastName, phoneNo } = req.body;
+
+  // Validate the request
+  if (!firstName || !lastName || !phoneNo) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  try {
+    const existingUser = await Registration.findOne({ phoneNo });
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    // Create a new registration document
+    const registration = new Registration({ firstName, lastName, phoneNo });
+
+    // Save the registration to the database
+    await registration.save();
+
+    return res.json({
+      success: true,
+      message: "Registered successfully",
     });
   } catch (error) {
     console.error(error);
